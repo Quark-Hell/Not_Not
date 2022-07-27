2a4b42f8a93d3e0beaef58ad3eef20d6de3530e3
Quark <gennadiy.kaydalov@mail.ru>
2022-07-27 23:24:13 +0300

    Optimization test

---
+++ Assets/_Scripts/ShowFPS.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour
+{
+    public static float fps;
+    GUIStyle style = new GUIStyle();
+
+    private void Start()
+    {
+        style.normal.textColor = Color.white;
+        style.fontSize = 32;
+        style.fontStyle = FontStyle.Bold;
+
+        Application.targetFrameRate = 300;
+    }
+
+    void OnGUI()
+    {
+        fps = 1.0f / Time.deltaTime;
+        GUI.Label(new Rect(10, 10, 100, 34), "FPS: " + (int)fps, style);
+    }
+}
