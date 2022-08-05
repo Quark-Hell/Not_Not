12f36727317a0709e6363a9ea75717c245fb2773
Quark <gennadiy.kaydalov@mail.ru>
2022-08-05 22:59:26 +0300

    BlackAndWhite Effect

---
+++ Assets/_Scripts/StartGame.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private Volume _volume;
+
+    [Header("Black And White Effect")]
+    [SerializeField] private float _zero;
+    [SerializeField] private float _normal;
+    [SerializeField] private float _grayscaleDuration;
+    private ColorAdjustments _colorAdjustments;
+
+    void Start()
+    {
+        GrayscaleEffect();
+    }
+
+    private void GrayscaleEffect()
+    {
+        if (_volume.profile.TryGet<ColorAdjustments>(out _colorAdjustments))
+        {
+            ClampedFloatParameter tmp = _colorAdjustments.saturation;
+
+            DOVirtual.Float(_zero, _normal, _grayscaleDuration, t => 
+            { 
+                tmp.value = t; 
+                _colorAdjustments.saturation = tmp; 
+            }).SetEase(Ease.InSine);
+        }
+    }
+}
